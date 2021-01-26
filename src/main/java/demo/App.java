68d083de1d877771f38ba1b036aec6847152f7ba
99263 <992635910@qq.com>
2021-01-26 16:55:29 +0800

    init

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -10,10 +10,15 @@ import java.util.logging.Logger;
 public class App {
     public static void main(String[] args) {
         Module mod = Module.load("demo-model.pt1");
+//        Tensor data =
+//                Tensor.fromBlob(
+//                        new float[]{1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f}, //data
+//                        new long[]{6,} //shape
+//                );
         Tensor data =
                 Tensor.fromBlob(
-                        new float[]{1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f}, //data
-                        new long[]{6,} //shape
+                        new double[]{1.0, 2.0, 3.0, 4.0, 5.0, 6.0} //data
+                        new long[]{6,}, //shape
                 );
 //        Tensor data =
 //                Tensor.fromBlob(
@@ -24,8 +29,8 @@ public class App {
         Tensor output = result.toTensor();
         Logger logger = Logger.getLogger("test");
         logger.info("shape: " + Arrays.toString(output.shape()));
-//        logger.info("data: " + Arrays.toString(output.getDataAsDoubleArray()));
-        logger.info("data: " + Arrays.toString(output.getDataAsFloatArray()));
+        logger.info("data: " + Arrays.toString(output.getDataAsDoubleArray()));
+//        logger.info("data: " + Arrays.toString(output.getDataAsFloatArray()));
 
         // Workaround for https://github.com/facebookincubator/fbjni/issues/25
         System.exit(0);

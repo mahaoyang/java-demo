bbd7ea72f2e01d9d070e596e039262d3de55b4a5
99263 <992635910@qq.com>
2021-01-27 17:38:57 +0800

    init

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -12,12 +12,12 @@ public class App {
         Module mod = Module.load("torch_weighted.pt");
         Tensor data =
                 Tensor.fromBlob(
-                        new double[]{1., 2., 3., 2., 6., 4., 4., 5., 6., 7., 8., 9.}, //data
-                        new long[]{4, 3} //shape
+                        new double[]{1., 2., 3., 2., 6., 4., 4., 5., 6., 7., 8., 9.}, //data 物料个数*特征个数n
+                        new long[]{4, 3} //shape 物料个数*特征个数n
                 );
         Tensor weight =
                 Tensor.fromBlob(
-                        new double[]{1.0, 1.0, 1.0}, //data
+                        new double[]{1.0, 1.0, 1.0}, //data 特征个数n
                         new long[]{1, 3} //shape
                 );
         IValue result = mod.forward(IValue.from(data), IValue.from(weight));

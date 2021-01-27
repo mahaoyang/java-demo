f53524a26f693a8038a7cf4672a44f044c9d4a05
99263 <992635910@qq.com>
2021-01-27 17:35:52 +0800

    init

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -12,8 +12,8 @@ public class App {
         Module mod = Module.load("torch_weighted.pt");
         Tensor data =
                 Tensor.fromBlob(
-                        new double[]{1., 2., 3., 2., 6., 4., 4., 5., 6.}, //data
-                        new long[]{3, 3} //shape
+                        new double[]{1., 2., 3., 2., 6., 4., 4., 5., 6., 7., 8., 9.}, //data
+                        new long[]{3, 4} //shape
                 );
         Tensor weight =
                 Tensor.fromBlob(
